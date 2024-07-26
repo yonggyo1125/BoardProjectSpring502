d36c4165f9a718a5a32c97eec2a7558361a8934c
yonggyo1125 <yonggyo00@kakao.com>
2024-07-26 16:01:17 +0900

    회원가입 구현중

--- src/main/java/org/choongang/global/configs/MvcConfig.java
+++ src/main/java/org/choongang/global/configs/MvcConfig.java
@@ -1,8 +1,23 @@
 package org.choongang.global.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
+
+
+    /**
+     * <input type="hidden" name="_method" value="PATCH">  -> PATCH 방식으로 요청
+     * ?_method=DELETE
+     * @return
+     */
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
