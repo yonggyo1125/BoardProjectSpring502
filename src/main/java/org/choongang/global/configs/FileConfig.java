a8ebfa4e791a4c21ff7e1df38c2abc9d015bb97c
yonggyo1125 <yonggyo00@kakao.com>
2024-07-26 15:39:44 +0900

    초기 설정 중

--- src/main/java/org/choongang/global/configs/FileConfig.java
+++ src/main/java/org/choongang/global/configs/FileConfig.java
@@ -1,21 +1,22 @@
 package org.choongang.global.configs;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
 public class FileConfig implements WebMvcConfigurer {
 
-    @Value("${file.upload.path}")
-    private String path;
-
-    @Value("${file.upload.url}")
-    private String url;
+   private final FileProperties properties;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(properties.getUrl() + "**")
+                .addResourceLocations("file:///" + properties.getPath());
 
     }
 }
