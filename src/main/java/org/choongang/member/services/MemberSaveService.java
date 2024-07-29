77137ea5276b7f7c440ce19cc52e2cf501a5f86b
yonggyo1125 <yonggyo00@kakao.com>
2024-07-29 15:53:50 +0900

    회원가입 검증

---
+++ src/main/java/org/choongang/member/services/MemberSaveService.java
@@ -0,0 +1,17 @@
+package org.choongang.member.services;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.member.repositories.AuthoritiesRepository;
+import org.choongang.member.repositories.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberSaveService {
+    private final MemberRepository memberRepository;
+    private final AuthoritiesRepository authoritiesRepository;
+    private final PasswordEncoder passwordEncoder;
+}
