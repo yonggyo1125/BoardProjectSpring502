f69209fd5a78848b5b46e2aecdfe07e704d0651b
yonggyo1125 <yonggyo00@kakao.com>
2024-07-29 15:20:05 +0900

    회원가입 검증

--- src/main/java/org/choongang/member/entities/Member.java
+++ src/main/java/org/choongang/member/entities/Member.java
@@ -1,6 +1,9 @@
 package org.choongang.member.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +15,18 @@ import org.choongang.global.entities.BaseEntity;
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class Member extends BaseEntity {
+    @Id @GeneratedValue
     private Long seq;
+
+    @Column(length=65, unique = true, nullable = false)
     private String email;
+
+    @Column(length=65, nullable = false)
     private String password;
+
+    @Column(length=40, nullable = false)
     private String userName;
+
+    @Column(length=15, nullable = false)
     private String mobile;
 }
