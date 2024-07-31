5bbe75527163460d11cd9f53d9a016ade59dee16
yonggyo1125 <yonggyo00@kakao.com>
2024-07-31 10:38:58 +0900

    스프링 시큐리티

--- src/main/java/org/choongang/member/services/LoginFailureHandler.java
+++ src/main/java/org/choongang/member/services/LoginFailureHandler.java
@@ -3,6 +3,8 @@ package org.choongang.member.services;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.choongang.member.controllers.RequestLogin;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -13,6 +15,14 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
     // 로그인 실패시에 유입되는 메서드
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
+        HttpSession session = request.getSession();
+
+        RequestLogin form = new RequestLogin();
+        form.setEmail(request.getParameter("email"));
+        form.setPassword(request.getParameter("password"));
+
+
+        session.setAttribute("requestLogin", form);
 
         // 로그인 실패시 로그인 페이지 이동
         response.sendRedirect(request.getContextPath() + "/member/login");
