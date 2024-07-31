4e1a959c36aa485885cdd7195f7fa88db75f22f7
yonggyo1125 <yonggyo00@kakao.com>
2024-07-31 09:34:37 +0900

    회원가입 검증

---
+++ src/main/java/org/choongang/member/services/MemberInfoService.java
@@ -0,0 +1,38 @@
+package org.choongang.member.services;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Authorities;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        List<Authorities> tmp = Objects.requireNonNullElse(member.getAuthorities(),
+                List.of(Authorities.builder().member(member).authority(Authority.USER).build()));
+
+        List<SimpleGrantedAuthority> authorities = tmp.stream()
+                .map(a -> new SimpleGrantedAuthority(a.getAuthority().name()))
+                .toList();
+
+
+        return null;
+    }
+}
