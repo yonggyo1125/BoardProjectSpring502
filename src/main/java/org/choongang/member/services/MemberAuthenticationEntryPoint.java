d1a3ea8739d7fdc350de1ae06a159b683bb40830
yonggyo1125 <yonggyo00@kakao.com>
2024-08-01 09:49:18 +0900

    스프링 시큐리티

--- src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
+++ src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
@@ -12,6 +13,22 @@ public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
-        System.out.println(authException);
+        /**
+         * 회원 전용 페이지로 접근한 경우 - /mypage -> 로그인 페이지 이동
+         * 관리자 페이지로 접근한 경우 - 응답 코드 401, 에러페이지 출력
+         */
+
+        String uri = request.getRequestURI();
+        if (uri.contains("/admin")) { // 관리자 페이지
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else { // 회원 전용 페이지
+            String qs = request.getQueryString();
+            String redirectUrl = uri.replace(request.getContextPath(), "");
+            if (StringUtils.hasText(qs)) {
+                redirectUrl += "?" + qs;
+            }
+
+            response.sendRedirect(request.getContextPath() + "/member/login?redirectUrl=" + redirectUrl);
+        }
     }
 }
