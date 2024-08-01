2ad2656cdd17bf747a47b19ac391d0b2e23b2d11
yonggyo1125 <yonggyo00@kakao.com>
2024-08-01 12:18:02 +0900

    에러 페이지 처리

--- src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -2,11 +2,28 @@ package org.choongang.global.advices;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.member.MemberUtil;
+import org.choongang.member.entities.Member;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @RequiredArgsConstructor
 @ControllerAdvice("org.choongang")
 public class CommonControllerAdvice {
 
     private final MemberUtil memberUtil;
+
+    @ModelAttribute("loggedMember")
+    public Member loggedMember() {
+        return memberUtil.getMember();
+    }
+
+    @ModelAttribute("isLogin")
+    public boolean isLogin() {
+        return memberUtil.isLogin();
+    }
+
+    @ModelAttribute("isAdmin")
+    public boolean isAdmin() {
+        return memberUtil.isAdmin();
+    }
 }
