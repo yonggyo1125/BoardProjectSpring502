aa4b2780eda4408add77d2a13a7d7689099fc3ad
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 09:44:54 +0900

    공통 소스 추가

---
+++ src/main/java/org/choongang/global/exceptions/RestExceptionProcessor.java
@@ -0,0 +1,35 @@
+package org.choongang.global.exceptions;
+
+import org.choongang.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RestExceptionProcessor {
+
+    @ExceptionHandler(Exception.class)
+    default ResponseEntity<JSONData> errorHandler(Exception e) {
+
+        Object message = e.getMessage();
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 500
+        if (e instanceof CommonException commonException) {
+            status = commonException.getStatus();
+
+            Map<String, List<String>> errorMessages = commonException.getErrorMessages();
+            if (errorMessages != null) message = errorMessages;
+        }
+
+        JSONData data = new JSONData();
+        data.setSuccess(false);
+        data.setMessage(message);
+        data.setStatus(status);
+
+        e.printStackTrace();
+
+        return ResponseEntity.status(status).body(data);
+    }
+}
