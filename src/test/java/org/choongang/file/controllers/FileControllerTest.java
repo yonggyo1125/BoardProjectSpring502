6e8643800248f8d02131f90c03be0731afdb3a20
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 12:01:31 +0900

    파일 공통 모듈

--- src/test/java/org/choongang/file/controllers/FileControllerTest.java
+++ src/test/java/org/choongang/file/controllers/FileControllerTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.multipart.MultipartFile;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -20,18 +22,24 @@ public class FileControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private MultipartFile[] files;
+    private MockMultipartFile file1;
+    private MockMultipartFile file2;
 
     @BeforeEach
     void init() {
-        MockMultipartFile file1 = new MockMultipartFile("file", "test1.png", "images/png", "ABC".getBytes());
-        MockMultipartFile file2 = new MockMultipartFile("file", "test2.png", "images/png", "DEF".getBytes());
+        file1 = new MockMultipartFile("file", "test1.png", "images/png", "ABC".getBytes());
+        file2 = new MockMultipartFile("file", "test2.png", "images/png", "DEF".getBytes());
 
-        files = new MultipartFile[] { file1, file2 };
     }
 
     @Test
     void uploadTest() throws Exception {
-        mockMvc.perform(multipart())
+        mockMvc.perform(multipart("/file/upload")
+                .file(file1)
+                .file(file2)
+                .param("gid", "testgid")
+                .param("location", "testlocation")
+                .with(csrf().asHeader())
+        ).andDo(print()).andExpect(status().isCreated());
     }
 }
