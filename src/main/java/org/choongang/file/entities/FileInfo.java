aef4cb75fa8d93b490a1729bd66d9d76a1e74f0e
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 10:15:10 +0900

    파일 공통 모듈

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -1,5 +1,6 @@
 package org.choongang.file.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,6 +10,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.choongang.global.entities.BaseMemberEntity;
 
+import java.util.UUID;
+
 @Data
 @Entity
 @Builder
@@ -17,10 +20,16 @@ public class FileInfo extends BaseMemberEntity {
     @Id @GeneratedValue
     private Long seq; // 서버에 업로드될 파일 이름  - seq.확장자
 
-    private String gid; // 그룹 ID
+    @Column(length=45, nullable = false)
+    private String gid = UUID.randomUUID().toString(); // 그룹 ID
+
+    @Column(length=45)
     private String location; // 그룹 안에 세부 위치
 
+    @Column(length=80, nullable = false)
     private String fileName;
+
+    @Column(length=80)
     private String contentType;
 
     private boolean done; // 그룹 작업 완료 여부
