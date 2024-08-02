38f527919c8da87141ec06558d7c21418302b1d7
yonggyo1125 <yonggyo00@kakao.com>
2024-08-02 12:36:19 +0900

    파일 공통 모듈

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -1,9 +1,6 @@
 package org.choongang.file.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -36,4 +33,10 @@ public class FileInfo extends BaseMemberEntity {
     private String contentType;
 
     private boolean done; // 그룹 작업 완료 여부
+
+    @Transient
+    private String fileUrl; // 파일 접근 URL
+
+    @Transient
+    private String filePath; // 파일 업로드 경로
 }
--- src/main/java/org/choongang/file/services/FileInfoService.java
+++ src/main/java/org/choongang/file/services/FileInfoService.java
@@ -3,6 +3,7 @@ package org.choongang.file.services;
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.constants.FileStatus;
 import org.choongang.file.entities.FileInfo;
+import org.choongang.file.exceptions.FileNotFoundException;
 import org.choongang.file.repositories.FileInfoRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +22,15 @@ public class FileInfoService {
      * @return
      */
     public FileInfo get(Long seq) {
+        FileInfo item = infoRepository.findById(seq).orElseThrow(FileNotFoundException::new);
 
-        return null;
+        /**
+         * 2차 가공
+         * 1. 파일을 접근할 수 있는 URL - 보여주기 위한 목적
+         * 2. 파일을 접근할 수 있는 PATH - 파일 삭제, 다운로드 등등
+         */
+
+        return item;
     }
 
     /**
