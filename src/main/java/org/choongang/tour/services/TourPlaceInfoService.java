afa94d91a7625111fe8aab97d56d287a8b5be18b
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 17:29:16 +0900

    공공 API

--- src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
+++ src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
@@ -1,11 +1,15 @@
 package org.choongang.tour.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.global.rests.gov.api.ApiItem;
+import org.choongang.global.rests.gov.api.ApiResult;
 import org.choongang.tour.controllers.TourPlaceSearch;
 import org.choongang.tour.entities.TourPlace;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -21,8 +25,17 @@ public class TourPlaceInfoService {
         int radius = search.getRadius();
 
         String url = String.format("https://apis.data.go.kr/B551011/KorService1/locationBasedList1?MobileOS=AND&MobileApp=test&mapX=%f&mapY=%f&radius=%d&numOfRows=5000&serviceKey=%s&_type=json", lon, lat, radius, serviceKey);
+        try {
+            ResponseEntity<ApiResult> response = restTemplate.getForEntity(URI.create(url), ApiResult.class);
 
+            if (response.getStatusCode().is2xxSuccessful() && response.getBody().getResponse().getHeader().getResultCode().equals("0000")) {
 
+                List<Long> ids = response.getBody().getResponse().getBody().getItems().getItem().stream().map(ApiItem::getContentid).toList();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
