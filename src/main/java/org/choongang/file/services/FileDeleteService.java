692f299137861e583e6d634cf18c50c3e84113d6
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 11:31:56 +0900

    파일 공통 모듈

--- src/main/java/org/choongang/file/services/FileDeleteService.java
+++ src/main/java/org/choongang/file/services/FileDeleteService.java
@@ -1,4 +1,29 @@
 package org.choongang.file.services;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
+import org.choongang.member.MemberUtil;
+import org.choongang.member.entities.Member;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+@RequiredArgsConstructor
 public class FileDeleteService {
+    private final FileInfoService infoService;
+    private final FileInfoRepository infoRepository;
+    private final MemberUtil memberUtil;
+
+    public FileInfo delete(Long seq) {
+        FileInfo data = infoService.get(seq);
+        String filePath = data.getFilePath();
+        String createdBy = data.getCreatedBy(); // 업로드한 회원 이메일
+
+        Member member = memberUtil.getMember();
+        if (!memberUtil.isAdmin() && StringUtils.hasText(createdBy) && memberUtil.isLogin() && !member.getEmail().equals(createdBy)) {
+
+        }
+    }
+
 }
