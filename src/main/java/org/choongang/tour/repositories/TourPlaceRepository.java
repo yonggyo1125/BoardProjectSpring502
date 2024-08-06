a9caba4c8d30571e9bda95e522ae4991de7a9f81
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 16:00:35 +0900

    공공 API

---
+++ src/main/java/org/choongang/tour/repositories/TourPlaceRepository.java
@@ -0,0 +1,8 @@
+package org.choongang.tour.repositories;
+
+import org.choongang.tour.entities.TourPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface TourPlaceRepository extends JpaRepository<TourPlace, Long>, QuerydslPredicateExecutor<TourPlace> {
+}
