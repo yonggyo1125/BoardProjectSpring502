283c2fa23c2e9d336b46b4cc9cac3e53a5073290
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 12:46:03 +0900

    파일 공통 모듈

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -2,15 +2,21 @@ package org.choongang.main.controllers;
 
 import org.choongang.global.exceptions.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainController implements ExceptionProcessor {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("addCommonScript", List.of("fileManager"));
+        model.addAttribute("addScript", List.of("test/form"));
 
         return "front/main/index";
     }
--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -26,3 +26,29 @@ const fileManager = {
 
     }
 };
+
+
+window.addEventListener("DOMContentLoaded", function() {
+    // 파일 업로드 버튼 이벤트 처리 S
+    const fileUploads = document.getElementsByClassName("fileUploads");
+    const fileEl = document.createElement("input");
+    fileEl.type = 'file';
+    fileEl.multiple = true;
+
+    for (const el of fileUploads) {
+        el.addEventListener("click", function() {
+            fileEl.value = "";
+
+            fileEl.click();
+
+        });
+    }
+    // 파일 업로드 버튼 이벤트 처리 E
+
+    // 파일 업로드 처리
+    fileEl.addEventListener("change", function(e) {
+        const files = e.target.files;
+        console.log(files);
+    });
+
+});
