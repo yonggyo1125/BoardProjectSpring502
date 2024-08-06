42df654525beed824d5f6a10e5ffa2138df876a7
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 17:34:43 +0900

    공공 API

--- src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
+++ src/main/java/org/choongang/tour/services/TourPlaceInfoService.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.choongang.global.rests.gov.api.ApiItem;
 import org.choongang.global.rests.gov.api.ApiResult;
 import org.choongang.tour.controllers.TourPlaceSearch;
+import org.choongang.tour.entities.QTourPlace;
 import org.choongang.tour.entities.TourPlace;
+import org.choongang.tour.repositories.TourPlaceRepository;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +15,15 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.data.domain.Sort.Order.asc;
+
 @Service
 @RequiredArgsConstructor
 public class TourPlaceInfoService {
 
     private final RestTemplate restTemplate;
+    private final TourPlaceRepository repository;
+
     private String serviceKey = "CHrWrFoNSLs09ec0iaNNKpj3VuYnP%2BJA5WAtHyPqdDVCdF%2Fn1NB46%2Bfd2NDjlTiNm%2Fw48BE9guQbOo12k2a6wA%3D%3D";
 
     public List<TourPlace> getList(TourPlaceSearch search) {
@@ -31,7 +38,13 @@ public class TourPlaceInfoService {
             if (response.getStatusCode().is2xxSuccessful() && response.getBody().getResponse().getHeader().getResultCode().equals("0000")) {
 
                 List<Long> ids = response.getBody().getResponse().getBody().getItems().getItem().stream().map(ApiItem::getContentid).toList();
-            }
+                if (!ids.isEmpty()) {
+                    QTourPlace tourPlace = QTourPlace.tourPlace;
+                    List<TourPlace> items = (List<TourPlace>)repository.findAll(tourPlace.contentId.in(ids), Sort.by(asc("contentId")));
+
+                    return items;
+                } // endif
+            } // endif
 
         } catch (Exception e) {
             e.printStackTrace();
