88a0da0da0f2f6856575ef028767c8577afc72c2
yonggyo1125 <yonggyo00@kakao.com>
2024-08-07 17:45:30 +0900

    지도 API

--- src/main/resources/static/front/js/tour/view.js
+++ src/main/resources/static/front/js/tour/view.js
@@ -9,6 +9,7 @@ window.addEventListener("DOMContentLoaded", function() {
             {lat: 37.557287959390024, lng: 126.94120499658828},
             {lat: 37.561184514897825, lng: 126.94069261563956},
         ],
+        markerImage: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png",
     };
     mapLib.load("map1", 1000, 600, options);
     /*
