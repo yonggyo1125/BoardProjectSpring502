2c2c4878fa15080d3793529470cb0dbe0633b662
yonggyo1125 <yonggyo00@kakao.com>
2024-08-07 15:35:55 +0900

    지도 API

---
+++ src/main/resources/static/front/js/tour/view.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const mapEl = document.getElementById("map");
+    mapEl.style.width = "1000px";
+    mapEl.style.height = "600px";
+
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+        const { latitude, longitude } = pos.coords;
+
+        const mapOption = {
+            center: new kakao.maps.LatLng(latitude, longitude);
+            level: 3,
+        };
+
+        const map = new kakao.maps.Map(mapEl, mapOption);
+
+    });
+
+});
