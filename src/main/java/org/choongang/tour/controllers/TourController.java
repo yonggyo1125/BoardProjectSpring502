f39214720d70f26592008c205b47ab4d3b923b84
yonggyo1125 <yonggyo00@kakao.com>
2024-08-07 14:56:04 +0900

    지도 API

--- src/main/java/org/choongang/tour/controllers/TourController.java
+++ src/main/java/org/choongang/tour/controllers/TourController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/tour")
 public class TourController implements ExceptionProcessor {
 
     @GetMapping("/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute("addCommonScript", List.of("map"));
+        model.addAttribute("addScript", List.of("tour/view"));
 
         return "front/tour/view";
     }
