6d74c83480d37553a0358d942b907296eb1f52c7
yonggyo1125 <yonggyo00@kakao.com>
2024-08-08 18:56:36 +0900

    지도 API

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -9,7 +9,8 @@ const mapLib = {
     *               - center: { lat: 위도, lng: 경도} - 필수
     *               - zoom : 확대 정도(1~10) / 숫자가 작을 수록 확대
                     - markerImage: 공통 마커 이미지 주소, 개별 마커 이미지가 있는 경우는 그걸로 대체,
-    *               - marker : [{ lat: 위도, lng: 경도, info: html 데이터(인포윈도우), image: 이미지 주소 - 마커이미지}]
+    *               - marker : [{ lat: 위도, lng: 경도,
+                                info: { content: html 데이터(인포윈도우), clickable: true|false - true(마커 클릭시 노출) }, image: 이미지 주소 - 마커이미지}]
     */
     load(mapId, width = 300, height = 300, options) {
         const mapEl = document.getElementById(mapId);
@@ -35,16 +36,36 @@ const mapLib = {
             if (!Array.isArray(marker)) marker = [marker];
 
             const markers = marker.map(m => {
-                const opt = { position: new kakao.maps.LatLng(m.lat, m.lng)};
+                const { lat, lng, image, info } = m;
+                const opt = { position: new kakao.maps.LatLng(lat, lng)};
+
 
                 // 마커 이미지 처리
-                const mi = markerImage ? markerImage : m.image;
+                const mi = markerImage ? markerImage : image;
                 if (mi) {
                     const mImage = new kakao.maps.MarkerImage(mi, new kakao.maps.Size(64, 69), {offset: new kakao.maps.Point(27, 69)});
                     opt.image = mImage;
                 }
 
                 const _marker = new kakao.maps.Marker(opt);
+
+                // 인포 윈도우 처리
+                if (info?.html) {
+                    const { html, clickable, removable } = info;
+
+                    const infoWindow = new kakao.maps.InfoWindow({
+                        content: html,
+                        removable: Boolean(removable),
+                    });
+
+                    if (clickable) { // 마커 클릭시 노출
+
+                    } else { // 바로 노출
+                        infoWindow.open(map, _marker);
+                    }
+                }
+
+
 
                 _marker.setMap(map);
 
--- src/main/resources/static/front/js/tour/view.js
+++ src/main/resources/static/front/js/tour/view.js
@@ -1,11 +1,12 @@
 window.addEventListener("DOMContentLoaded", function() {
+    const html = "<h1>윈포윈도우 테스트</h1>";
     const options = {
         center: {
             lat: 37.557756188912954,
             lng: 126.94062742683245,
         },
         marker: [
-            {lat: 37.557756188912954, lng: 126.94062742683245},
+            {lat: 37.557756188912954, lng: 126.94062742683245, info: { content: html, clickable: false, removable: true }},
             {lat: 37.557287959390024, lng: 126.94120499658828},
             {lat: 37.561184514897825, lng: 126.94069261563956},
         ],
